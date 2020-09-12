384fbe7ddd0d7c666faaf95b9b91ad018cdb1035
Eliud Kagema <eliud.k.maina@gmail.com>
2020-09-12 11:13:35 +0300

    second commit

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ app = FastAPI()
 
 @app.get("/")
 async def dashboard():
+    """
+    displays the stock screener dashboard/ homepage
+
+    """
     return {"Dashboard": "Home Page"}
 
 
 @app.post("/stock")
 def create_stock():
+    """
+    create a stock and stores in the database
+
+    """
     return {
         "code": "success",
-        "message": "created"
+        "message": "created",
     }
